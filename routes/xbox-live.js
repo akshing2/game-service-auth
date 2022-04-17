f3cbeae3fd74206ee888cc5e0787f0d0d267616b
Akshin Goswami <akshin.goswami@gmail.com>
2022-04-17 14:58:05 +1000

    added code formatter

--- routes/xbox-live.js
+++ routes/xbox-live.js
@@ -11,33 +11,37 @@ const router = express.Router();
 const xboxLiveAuth = require('@xboxreplay/xboxlive-auth');
 
 // handle authentication requests for xbox live accounts
-router.post('/api/xbox/authenticate', (req, res) => { 
+router.post('/api/xbox/authenticate', (req, res) => {
     console.log('Request Body: ', req);
 
     // email and password from request
     const xboxEmail = req.body.email;
     const xboxPassword = req.body.password;
 
-    // TODO: Create a validation method for incoming requests 
+    // TODO: Create a validation method for incoming requests
 
-    console.log('Attempting xbox live authentication with following credentials...');
+    console.log(
+        'Attempting xbox live authentication with following credentials...',
+    );
     console.log('email: ', xboxEmail);
     console.log('password: ', xboxPassword);
 
     // now authenticate
-    xboxLiveAuth.authenticate(xboxEmail, xboxPassword)
-    .then((responseFromXbox) => {
-        // authentication successful so console log response
-        console.log(responseFromXbox);
-        res.status(200).send(responseFromXbox);
-    })
-    .catch((error)=> {
-        // authentication unsuccessful so send error
-        console.warn('ERROR - Authentication from Xbox Live Failed: ', error);
-        res.status(400).send(error);
-    })
-
-})
+    xboxLiveAuth
+        .authenticate(xboxEmail, xboxPassword)
+        .then((responseFromXbox) => {
+            // authentication successful so console log response
+            console.log(responseFromXbox);
+            res.status(200).send(responseFromXbox);
+        })
+        .catch((error) => {
+            // authentication unsuccessful so send error
+            console.warn(
+                'ERROR - Authentication from Xbox Live Failed: ',
+                error,
+            );
+            res.status(400).send(error);
+        });
+});
 
 module.exports = router;
-
